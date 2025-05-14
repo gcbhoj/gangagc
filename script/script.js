55eb8c6a0a4f1fded9083c467bc3ae87f8a980c5
BhojGC <ghartich@sheridancollege.ca>
2025-05-13 23:00:40 -0400

    fixed js for language toggle. added media query to remove header user controls below 860 px

--- script/script.js
+++ script/script.js
@@ -6,21 +6,22 @@ window.addEventListener("load", () => {
   // Set default English content
   english_version.textContent = "Carry On inside this loop";
   english_version.style.display = "block";
-  test_display.style.visibility = "hidden";
+  test_display.style.display = "none";
 
   language_selection.addEventListener("change", (e) => {
     const selectedLanguage = e.target.value;
 
     if (selectedLanguage === "ne") {
-      test_display.style.visibility = "visible";
+      test_display.style.display = "block";
       test_display.textContent = "Site Under Construction";
       english_version.style.display = "none";
       console.log("Nepali Language Selected");
     } else {
-      test_display.style.visibility = "hidden";
+      test_display.style.display = "none";
       english_version.style.display = "block";
       english_version.textContent = "Carry On inside this loop";
       console.log("English or default selected");
     }
   });
+
 });
